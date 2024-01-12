d5943744bb1ce6d55bc6951c8e4f68ad095b297d
Luis Magalhaes <luis.magalhaes@theblueground.com>
2024-01-12 19:37:06 -0300

    Fix database deployment

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_11_154047) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false

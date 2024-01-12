69966539ad480750effb277a7ead15df5938914d
Luis Magalhaes <luis.magalhaes@theblueground.com>
2024-01-12 20:12:26 -0300

    Change how to retrieve the IP from user

--- app/controllers/search_terms_controller.rb
+++ app/controllers/search_terms_controller.rb
@@ -1,6 +1,10 @@
 class SearchTermsController < ApplicationController
   def index
-    ip = request.remote_ip
+    ip = real_ip
     @search_terms = SearchTerm.where(ip: ip).order(count: :desc)
   end
+end
+
+def real_ip
+  request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
 end
